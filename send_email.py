1de6527da2bdc8703d1a7298aafd1e96a78e2b5c
Nayem Uddin <nurakib.nstu@gmail.com>
2019-06-25 23:18:25 +0600

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -3,7 +3,7 @@ import smtplib
 
 def send_email(email, height, average_height, count):
     from_email="nurakib.survey@gmail.com"
-    from_password="thisisPassworD"
+    from_password="thisisBLAHblah"
     to_email=email
 
     subject="Height Data"
